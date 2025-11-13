41025a9ca5c3602a67df0549bc74488ef5f6a781
George geham <george25gehamnew@gmail.com>
2025-11-13 21:05:45 +0200

    Merge

--- Learnify_API/Learnify_API/obj/Release/net8.0/Learnify_API.AssemblyInfo.cs
+++ Learnify_API/Learnify_API/obj/Release/net8.0/Learnify_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learnify_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4ac6e48fc8055ac67202855f71e3fb2133e26fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57a67fd69b12b474808e1ee0a78ad674b736eded")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learnify_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learnify_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
