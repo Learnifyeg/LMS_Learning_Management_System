a12833fd34466457b97c5a53557d30c7e91efe07
Rana <ranauag2004@gmail.com>
2025-11-15 05:33:49 +0200

    ..

--- Learnify_API/Learnify_API/obj/Release/net8.0/Learnify_API.AssemblyInfo.cs
+++ Learnify_API/Learnify_API/obj/Release/net8.0/Learnify_API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learnify_API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+941cdb0ebfba3fc9a3779baa039724f220659436")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learnify_API")]
